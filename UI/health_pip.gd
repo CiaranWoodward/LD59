5681df57f4e4ce0cbde9c3ee1fd9cedc99e21dcc
Mai-Lan Vu <miavu11@gmail.com>
2026-04-20 08:42:39 +0100

    Added font, may change...

--- UI/health_pip.gd
+++ UI/health_pip.gd
@@ -7,23 +7,23 @@ extends Node2D
 @onready var animationTree = $States/AnimationTree["parameters/playback"] as AnimationNodeStateMachinePlayback
 
 func _ready() -> void:
-    # Initial state
-    animationTree.start("Start")
-    if Global.statistics[pip_type] >= pip_threshold:
-        await get_tree().create_timer(pip_inital_delay_multiplier * pip_threshold).timeout
-        _turn_on()
-    # Listener for future changes
-    Global.statistic_changed.connect(func(stat, new_value, _old_value):
-        if stat == pip_type:
-            if new_value >= pip_threshold and _old_value < pip_threshold:
-                _turn_on()
-            elif new_value < pip_threshold and _old_value >= pip_threshold:
-                _turn_off()
-    )
+	# Initial state
+	animationTree.start("Start")
+	if Global.statistics[pip_type] >= pip_threshold:
+		await get_tree().create_timer(pip_inital_delay_multiplier * pip_threshold).timeout
+		_turn_on()
+	# Listener for future changes
+	Global.statistic_changed.connect(func(stat, new_value, _old_value):
+		if stat == pip_type:
+			if new_value >= pip_threshold and _old_value < pip_threshold:
+				_turn_on()
+			elif new_value < pip_threshold and _old_value >= pip_threshold:
+				_turn_off()
+	)
 
 
 func _turn_on():
-    animationTree.travel("Filling")
+	animationTree.travel("Filling")
 
 func _turn_off():
-    animationTree.travel("Emptying")
+	animationTree.travel("Emptying")
